138939f839a30c55c3b420ab5223a84a8bd9565f
Elena-code-dev <kev.76@inbox.ru>
2022-12-06 00:35:20 +0300

    feat: creat.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.access('files/fresh.txt', constants.F_OK);
+        console.log('FS operation failed')
+    } catch { 
+        fs.writeFile('files/fresh.txt', 'I am fresh and young');
+    }
 };
 
 await create();
