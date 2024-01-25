cfa99bdc2dbe1d45c15f3d6c52888e46f1dc7b1d
Elena-code-dev <kev.76@inbox.ru>
2024-01-25 21:14:41 +0300

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { writeFile, access} from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+
+    access('fs/files/fresh.txt', (err) =>  {
+        if(err) {
+            writeFile('fs/files/fresh.txt', 'I am fresh and young', (err) => (err))
+        } else {
+            console.log('FS operation failed');
+        }
+    })
 };
 
 await create();
